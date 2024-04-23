b53d80ad789dd389998e445a5a2b0d92ca94a4f8
RogerCodes <rogelioherreradev@gmail.com>
2024-04-23 10:39:56 -0600

    Added Data context class

---
+++ Short_It/Data/ShortItAppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Short_It.Models;
+
+namespace Short_It.Data
+{
+    public class ShortItAppContext : DbContext
+    {
+
+        public ShortItAppContext(DbContextOptions<ShortItAppContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Link> Links { get; set; }
+
+    }
+}
