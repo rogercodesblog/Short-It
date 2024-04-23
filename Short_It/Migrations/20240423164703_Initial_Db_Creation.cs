2f7460d62232fac754bbead606420d6a9fd1a482
RogerCodes <rogelioherreradev@gmail.com>
2024-04-23 10:48:27 -0600

    Added first entity framework migration, created application database

---
+++ Short_It/Migrations/20240423164703_Initial_Db_Creation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Short_It.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Db_Creation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LinkTitle = table.Column<string>(type: "TEXT", nullable: false),
+                    FullLink = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortLink = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Links");
+        }
+    }
+}
