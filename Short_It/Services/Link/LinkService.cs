62e03334bf075b0bb7fd11990edcda4486fc2517
RogerCodes <rogelioherreradev@gmail.com>
2024-04-24 11:07:02 -0600

    Implemented Interface 'LinkService'

--- Short_It/Services/Link/LinkService.cs
+++ Short_It/Services/Link/LinkService.cs
@@ -1,6 +1,22 @@
-﻿namespace Short_It.Services.Link
+﻿using Short_It.DTOs.Link;
+
+namespace Short_It.Services.Link
 {
     public class LinkService : ILinkService
     {
+        public Task<ServiceResponse<LinkDTO>> AddLinkAsync(CreateLinkDTO createLinkDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<LinkDTO>> DeleteLinkAsync(LinkDTO linkDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<LinkDTO>> GetLinkByShortUrlAsync(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
