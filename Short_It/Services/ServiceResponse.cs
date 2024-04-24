9dc26dc2d53e255121a3c817bbc4abb85b994fd4
RogerCodes <rogelioherreradev@gmail.com>
2024-04-24 09:36:45 -0600

    Added ServiceResponse class

---
+++ Short_It/Services/ServiceResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Short_It.Services
+{
+    public class ServiceResponse<T>
+    {
+        public T Data { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public List<string> ErrorMessagees { get; set; } = null;
+    }
+}
