b0035726e42ddec0b6dae3cb00ce7f00f8a6814c
RogerCodes <rogelioherreradev@gmail.com>
2024-04-24 11:06:16 -0600

    Added Interface for Link Service

--- Short_It/Services/Link/ILinkService.cs
+++ Short_It/Services/Link/ILinkService.cs
@@ -1,7 +1,12 @@
-﻿namespace Short_It.Services.Link
+﻿using Microsoft.AspNetCore.SignalR.Protocol;
+using Short_It.DTOs.Link;
+
+namespace Short_It.Services.Link
 {
     public interface ILinkService
     {
-
+        Task<ServiceResponse<LinkDTO>> AddLinkAsync(CreateLinkDTO createLinkDTO);
+        Task<ServiceResponse<LinkDTO>> GetLinkByShortUrlAsync(string shortUrl);
+        Task<ServiceResponse<LinkDTO>> DeleteLinkAsync(LinkDTO linkDTO);
     }
 }
