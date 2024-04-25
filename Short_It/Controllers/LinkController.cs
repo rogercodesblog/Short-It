9e5948f5f40b7a67aa097637eccc81d0d15fddef
RogerCodes <rogelioherreradev@gmail.com>
2024-04-25 08:48:52 -0600

    Added controller for Links

---
+++ Short_It/Controllers/LinkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Short_It.Controllers
+{
+    public class LinkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
