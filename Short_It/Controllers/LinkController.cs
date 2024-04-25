3552ac16ca5e1676df8dad57119ecf03a371ad61
RogerCodes <rogelioherreradev@gmail.com>
2024-04-25 12:23:57 -0600

    Implemented 'CreateShortLink' method on 'LinkController'

--- Short_It/Controllers/LinkController.cs
+++ Short_It/Controllers/LinkController.cs
@@ -12,5 +12,33 @@ namespace Short_It.Controllers
             _linkService = linkService;
         }
 
+        [HttpPost("[action]")]
+        [ProducesResponseType(StatusCodes.Status200OK,Type = typeof(LinkDTO))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<LinkDTO>> CreateShortLink([FromBody]CreateLinkDTO createLinkDTO)
+        {
+
+            if(createLinkDTO == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var _newLink = await _linkService.AddLinkAsync(createLinkDTO);
+
+            if (_newLink.Success == false)
+            {
+                ModelState.AddModelError("", _newLink.Message);
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok(_newLink);
+
+        }
+
     }
 }
