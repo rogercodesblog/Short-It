cd58e471fb1732e995c37f93a9c2c1459cb15015
RogerCodes <rogelioherreradev@gmail.com>
2024-04-27 16:48:40 -0600

    Elaborated more on js script, now the messages are shown on the page

--- Short_It/wwwroot/js/site.js
+++ Short_It/wwwroot/js/site.js
@@ -1,29 +1,73 @@
-﻿
+﻿let labelResult = $('#labelResult');
+let btnGenerateShortLink = $('#btnGenerate');
+let inputUrl;
+let ApicallResponseErrors;
+let ApicallResponseErrorsText;
+let ApiCallErrorDefaultResponseText = 'There was an error while creating the short link, please try again.';
+let txtInputEmptyErrorMessage = 'The provided url can\'t be empty';
+
 function GenerateShortLink() {
-    var inputUrl = document.getElementById("txtUrl").value;
+    inputUrl = $("#txtUrl").val();
+    isButtonEnabled(false);
 
-    //Todo: Validate if input url is null or not
-
-
-    var createlink = {
-        FullLink: inputUrl
-    };
-
-    var json = JSON.stringify(createlink);
+    if (IsUrlEmpty(inputUrl)) {
+        ShowResult(false, txtInputEmptyErrorMessage);
+        return;
+    }
 
     $.ajax({
-        type: "POST",
-        url: "/Link/CreateShortLink",
-        data: json,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (msg) {
-            alert('It Worked!');
+        url: '/Link/CreateShortLink',
+        type: 'POST',
+        contentType: 'application/json; charset=utf-8',
+        datatype: 'JSON',
+        data:
+            JSON.stringify({
+                'FullLink': inputUrl
+            }),
+        success: function (textStatus) {
+            //Todo: Show correct message and new link
+            ShowResult(true, textStatus);
         },
-        error: function(msg) {
-            alert('It didn\'t worked :(');
+        error: function (xhr) {
+
+            if (xhr.status === 400) {
+
+                ApicallResponseErrors = JSON.parse(xhr.responseText);
+
+                for (var key in ApicallResponseErrors) {
+                    console.log(`Incorrect value for ${key}: ${ApicallResponseErrors[key]}`);
+                    ApicallResponseErrorsText = `${ApicallResponseErrors[key]}`;
+                }
+
+                ShowResult(false, ApicallResponseErrorsText);
+            }
+            else if (xhr.status === 500) {
+                ShowResult(false, xhr.responseText);
+            }
+            else {
+                ShowResult(false, ApiCallErrorDefaultResponseText);
+            }
         }
     });
 
-  
+}
+
+function ShowResult(Success, Message) {
+    if (Success) {
+        labelResult.text(Message);
+        labelResult.removeClass("d-none").addClass("d-block").addClass("alert-success");
+    }
+    else {
+        labelResult.text(Message);
+        labelResult.removeClass("d-none").addClass("d-block").addClass("alert-danger");
+    }
+    isButtonEnabled(true);
+}
+
+function IsUrlEmpty(url) {
+    return url == '' ? true : false;
+}
+
+function isButtonEnabled(state) {
+    btnGenerateShortLink.prop('disabled', state ? false : true);
 }
