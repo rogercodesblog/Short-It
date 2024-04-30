76c2c80481cd959a8ef984731730e0e32324d623
RogerCodes <rogelioherreradev@gmail.com>
2024-04-30 16:25:24 -0600

    The app now displays the full information and server response in a more user - friendly way

--- Short_It/wwwroot/js/site.js
+++ Short_It/wwwroot/js/site.js
@@ -1,17 +1,22 @@
-﻿let labelResult = $('#labelResult');
+﻿let resultContainer = $('#resultContainer');
+let resultContainerText = $('#resultContainerText');
+let resultContainerNewLinkInfo = $('#newLink');
 let btnGenerateShortLink = $('#btnGenerate');
+let linkFullUrl = $('#linkFullUrl');
+let linkShortUrl = $('#linkShortUrl');
 let inputUrl;
-let ApicallResponse;
-let ApicallResponseText;
-let ApiCallErrorDefaultResponseText = 'There was an error while creating the short link, please try again.';
+let generatedShortUrl;
+let apiCallResponse;
+let apiCallResponseText;
+let apiCallErrorDefaultResponseText = 'There was an error while creating the short link, please try again.';
 let txtInputEmptyErrorMessage = 'The provided url can\'t be empty';
 
-function GenerateShortLink() {
+function generateShortLink() {
     inputUrl = $("#txtUrl").val();
     isButtonEnabled(false);
 
-    if (IsUrlEmpty(inputUrl)) {
-        ShowResult(false, txtInputEmptyErrorMessage);
+    if (isUrlEmpty(inputUrl)) {
+        showResult(false, txtInputEmptyErrorMessage);
         return;
     }
 
@@ -26,53 +31,64 @@ function GenerateShortLink() {
             }),
         success: function (response) {
 
-            //TODO: Add a better way to see the "new short link"
-
-            ShowResult(true, `${response.message} \n Short Link: ${response.data.shortLink}` );
-
+            generatedShortUrl = response.data.shortLink
+            showResult(true, response.message);
         },
         error: function (xhr) {
 
             if (xhr.status === 400) {
 
-                ApicallResponse = JSON.parse(xhr.responseText);
+                apiCallResponse = JSON.parse(xhr.responseText);
 
-                for (var key in ApicallResponse) {
-                    console.log(`Incorrect value for ${key}: ${ApicallResponse[key]}`);
-                    ApicallResponseText = `${ApicallResponse[key]}`;
+                for (var key in apiCallResponse) {
+                    console.log(`Incorrect value for ${key}: ${apiCallResponse[key]}`);
+                    apiCallResponseText = `${apiCallResponse[key]}`;
                 }
 
-                ShowResult(false, ApicallResponseText);
+                showResult(false, apiCallResponseText);
             }
             else if (xhr.status === 500) {
-                ShowResult(false, xhr.responseText);
+                showResult(false, xhr.responseText);
             }
             else {
-                ShowResult(false, ApiCallErrorDefaultResponseText);
+                showResult(false, apiCallErrorDefaultResponseText);
             }
         }
     });
 
 }
 
-function ShowResult(Success, Message) {
-    labelResult.removeClass("d-none").addClass("d-block");
+function showResult(Success, Message) {
+
+    resultContainer.removeClass("d-none").addClass("d-block");
+
     if (Success) {
-        labelResult.text(Message);
-        labelResult.removeClass("alert-danger").addClass("alert-success");
+        resultContainer.removeClass("alert-danger").addClass("alert-success");
+        resultContainerText.text(Message);
+        resultContainerNewLinkInfo.removeClass('d-none');
+        setLinkValues(inputUrl, `${$(location).attr('href')}${generatedShortUrl}`);
     }
     else {
-        labelResult.text(Message);
-        labelResult.removeClass("alert-success").addClass("alert-danger");
+        resultContainer.removeClass("alert-success").addClass("alert-danger");
+        resultContainerText.text(Message);
+        resultContainerNewLinkInfo.addClass('d-none');
+        setLinkValues("", "");
     }
     isButtonEnabled(true);
 }
 
-function IsUrlEmpty(url) {
+function isUrlEmpty(url) {
     return url == '' ? true : false;
 }
 
 function isButtonEnabled(state) {
     btnGenerateShortLink.text(state ? "Short It!" : "Generating...");
     btnGenerateShortLink.prop('disabled', state ? false : true);
+}
+
+function setLinkValues(fullLink, shortLink) {
+    linkFullUrl.attr("href", fullLink);
+    linkFullUrl.text(fullLink);
+    linkShortUrl.attr("href", shortLink);
+    linkShortUrl.text(shortLink);
 }
