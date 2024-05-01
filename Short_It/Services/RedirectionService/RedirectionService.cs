32114a56f60ae79047441ad15d9d8062cdfe9bfb
RogerCodes <rogelioherreradev@gmail.com>
2024-05-01 10:11:49 -0600

    Implemented 'GetLinkToRedirectByShortUrlAsync' in 'RedirectionService.cs'

--- Short_It/Services/RedirectionService/RedirectionService.cs
+++ Short_It/Services/RedirectionService/RedirectionService.cs
@@ -1,5 +1,7 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
 using Short_It.Data;
+using Short_It.DTOs.Link;
 
 namespace Short_It.Services.RedirectionService
 {
@@ -12,9 +14,40 @@ namespace Short_It.Services.RedirectionService
             _database = database;
         }
 
-        public Task<ServiceResponse<string>> GetLinkToRedirectByShortUrlAsync(string shortUrl)
+        public async Task<ServiceResponse<string>> GetLinkToRedirectByShortUrlAsync(string shortUrl)
         {
-            throw new NotImplementedException();
+
+            ServiceResponse<string> _response = new ServiceResponse<string>();
+
+            try
+            {
+                var _linkToReturn = await _database.Links.FirstOrDefaultAsync(shortLink => shortLink.ShortLink == shortUrl);
+
+                if (_linkToReturn == null)
+                {
+                    _response.Success = false;
+                    _response.Data = null;
+                    _response.Message = "The requested link does not exists";
+                    return _response;
+                }
+
+                _response.Success = true;
+                _response.Data = _linkToReturn.FullLink;
+                _response.Message = "The link was fetch successfully";
+
+                _linkToReturn.TimesVisited++;
+                await _database.SaveChangesAsync();
+
+            }
+            catch (Exception)
+            {
+                _response.Success = false;
+                _response.Data = null;
+                _response.Message = "There was an internal server error, try again, if the error persist please do comunicate with our it staff";
+            }
+
+            return _response;
+
         }
 
     }
