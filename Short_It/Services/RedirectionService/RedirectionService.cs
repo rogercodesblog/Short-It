776828addde5d17ab99a821d0ebfa0d8c1e8152a
RogerCodes <rogelioherreradev@gmail.com>
2024-05-01 10:06:43 -0600

    Injected Database Context in RedirectionService

--- Short_It/Services/RedirectionService/RedirectionService.cs
+++ Short_It/Services/RedirectionService/RedirectionService.cs
@@ -1,11 +1,21 @@
 ﻿
+using Short_It.Data;
+
 namespace Short_It.Services.RedirectionService
 {
     public class RedirectionService : IRedirectionService
     {
+
+        private readonly ShortItAppContext _database;
+        public RedirectionService(ShortItAppContext database)
+        {
+            _database = database;
+        }
+
         public Task<ServiceResponse<string>> GetLinkToRedirectByShortUrlAsync(string shortUrl)
         {
             throw new NotImplementedException();
         }
+
     }
 }
